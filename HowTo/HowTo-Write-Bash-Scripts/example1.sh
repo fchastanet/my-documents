8cfa5b3a21ab913ab0fd466d0794669cb3da0b67
fchastanet <237869+fchastanet@users.noreply.github.com>
2025-01-04 23:49:19 +0000

    Auto-update lint fixes

--- HowTo/HowTo-Write-Bash-Scripts/example1.sh
+++ HowTo/HowTo-Write-Bash-Scripts/example1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 declare output status
 # shellcheck disable=SC2034,SC2154
-output="$(functionThatOutputSomething "${arg1}")"; status=$?
+output="$(functionThatOutputSomething "${arg1}")"
+status=$?
 echo "${status}"
